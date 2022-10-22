6bbc39366fd137d57ed2bef2239b57d583936f3e
RezyserKinaAkcji <michalpilarski17@gmail.com>
2022-10-22 21:48:33 +0200

    New model and register on admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Games
+
 # Register your models here.
+
+class GamesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date_created']
+    ordering = ['date_created']
+
+
+admin.site.register(Games, GamesAdmin)
