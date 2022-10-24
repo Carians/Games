5fb18927ad66500234fb980f9dbc595d52229b94
Carians <37939283+Carians@users.noreply.github.com>
2022-10-24 11:35:23 +0200

    Added link field to database model

--- main/models.py
+++ main/models.py
@@ -9,6 +9,7 @@ class Games(models.Model):
     description = models.TextField(null=False, blank=False)
     text = models.TextField(null=False, blank=False)
     date_created = models.DateField(default=timezone.now, blank=False, null=False)
+    link = models.URLField(max_length=200, null=False, blank=False, default='')
 
     def __str__(self):
         return f'{self.title} {self.description}'
