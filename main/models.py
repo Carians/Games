891d942915aa1cb9372c5a033690996466335aaf
Carians <37939283+Carians@users.noreply.github.com>
2022-10-24 13:23:43 +0200

    Added imgURL field to main/models

--- main/models.py
+++ main/models.py
@@ -8,7 +8,8 @@ class Games(models.Model):
     description = models.TextField(null=False, blank=False)
     text = models.TextField(null=False, blank=False)
     date_created = models.DateField(default=timezone.now, blank=False, null=False)
-    link = models.URLField(max_length=200, null=False, blank=False, default='')
+    link = models.URLField(max_length=200, null=False, blank=False)
+    imgURL = models.URLField()
 
     def __str__(self):
         return f'{self.title} {self.description}'
