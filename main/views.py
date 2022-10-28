5aa9bb19c13d382a65be3e0153267cf927b14b4f
RezyserKinaAkcji <michalpilarski17@gmail.com>
2022-10-28 12:25:21 +0200

    Added images to cards

--- main/views.py
+++ main/views.py
@@ -8,7 +8,7 @@ from .metadata import getMetaData
 # Create your views here.
 class homePageView(View):
     def get(self, request):
-        games = Games.objects.all().order_by('date_created')[:3]
+        games = Games.objects.all().order_by('-date_created')[:3]
         return render(request, 'main/main.html', {
             'games': games
         })
