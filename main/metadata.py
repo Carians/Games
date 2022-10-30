88b291324125f84572490711db2e594c7f6e8b7e
Carians <37939283+Carians@users.noreply.github.com>
2022-10-30 17:18:50 +0100

    Resolved some security issued at metadata.py

--- main/metadata.py
+++ main/metadata.py
@@ -4,15 +4,28 @@ import metadata_parser
 class getMetaData():
 
     def title(url):
-        page = metadata_parser.MetadataParser(url=str(url), search_head_only=True)
-        title = page.get_metadatas('title')
-        return title[0]
+        try:
+            page = metadata_parser.MetadataParser(url=str(url), search_head_only=True)
+            title = page.get_metadatas('title')
+            return title[0]
+        except:
+            pass
+        return None
+
 
     def image(url):
-        page = metadata_parser.MetadataParser(url=str(url), search_head_only=True)
-        image = page.get_metadatas('image')
-        return image[0]
+        try:
+            page = metadata_parser.MetadataParser(url=str(url), search_head_only=True)
+            image = page.get_metadatas('image')
+            return image[0]
+        except:
+            pass
+        return None
 
     def all(url):
-        page = metadata_parser.MetadataParser(str(url), search_head_only=True)
-        return str(page.metadata)
+        try:
+            page = metadata_parser.MetadataParser(str(url), search_head_only=True)
+            return str(page.metadata)
+        except:
+            pass
+        return None
