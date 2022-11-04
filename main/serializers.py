78d1866785740be8538f8df85b04e4307033cb5e
Carians <37939283+Carians@users.noreply.github.com>
2022-11-04 17:00:24 +0100

    Created basic api view and changed .gitignore

---
+++ main/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Games
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Games
+        fields = [
+            "title",
+            "description",
+            "text",
+            "date_created",
+            "link",
+            "imgURL"
+        ]
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'main'
 urlpatterns = [
  path('', views.homePageView.as_view(), name="homepage"),
  path('library', views.libraryView.as_view(), name="library"),
-
+ path('api', include('api.urls'))
 ]
