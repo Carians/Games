90e9373006e8709c9eb4314be3e6645a9ebfd8b2
RezyserKinaAkcji <michalpilarski17@gmail.com>
2022-11-15 17:05:59 +0100

    Delete search bar from detail and main page

--- main/views.py
+++ main/views.py
@@ -18,6 +18,11 @@ class libraryView(ListView):
     model = Games
     ordering = ['date_created']
     context_object_name = 'games'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_searchable'] = True
+        return context
 
 
 class singleGameView(DetailView):
