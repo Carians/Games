98320df744c6c05142370fe6ce14802851909112
Carians <37939283+Carians@users.noreply.github.com>
2023-02-14 21:37:00 +0100

    Fixed bug in add.js: Made script to autodetect site URL

--- main/static/main/includes/scripts/add.js
+++ main/static/main/includes/scripts/add.js
@@ -1,25 +1,43 @@
-const form = document.querySelector('#addForm')
+const form = document.querySelector('#addForm');
+const btn = document.querySelector('#send');
+let website_url = 'http://' + window.location.host;
 
-const btn = document.querySelector('#send')
+const loaderIcon = '<i class="fas fa-spinner fa-spin"></i>'; // ikona ładowania
 
 btn.addEventListener('click', ()=>{
-    const csrf_token = document.cookie.split('=')[1]
+    const csrf_token = document.cookie.split('=')[1];
 
-    game_title = form.children[1].value
-    email = form.children[3].value
-    url = form.children[5].value
-   
-    fetch('http://127.0.0.1:8000/api/games', {
+    const game_title = form.children[1].value;
+    const email = form.children[3].value;
+    const url = form.children[5].value;
+
+    btn.disabled = true; // blokowanie przycisku
+    btn.innerHTML = `${loaderIcon} Wysyłanie...`; // zmiana tekstu przycisku na ikonę ładowania
+
+    fetch(`${website_url}/api/games`, {
         method: 'POST',
         headers: {
             "Content-Type" : "application/json",
-            'X-CSRFToken': csrf_token
+            'X-CSRFToken': csrf_token,
         },
         body: JSON.stringify({
             title: game_title,
             link: url
         })
     })
-    .then(res => console.log(res.json()))
-    .catch(err => console.log(err))
-})
+    .then(res => {
+        console.log(res.json());
+        // odblokuj przycisk i usuń ikonę ładowania po pomyślnym wykonaniu żądania POST
+        if(res.status === 201) {
+            btn.disabled = false;
+            btn.innerHTML = 'Wyślij';
+            window.close();
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        // odblokuj przycisk i usuń ikonę ładowania w przypadku błędu
+        btn.disabled = false;
+        btn.innerHTML = 'Wyślij';
+    });
+});
